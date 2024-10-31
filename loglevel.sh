ac376d6491e818f3f0fef619220f8fee55735c35
furkangurbuz5 <furkangurbuz5@gmail.com>
2024-10-31 22:07:55 +0100

    fixes to logs

--- loglevel.sh
+++ loglevel.sh
@@ -1,8 +1,9 @@
 #changing debian loglevel because it annoys me during boot 
 
 sudo nano /etc/default/grub
-#change GRUB_CMDLINE_LINUX line to include loglevel=0
-#we can also add "quiet splash" for a boot splash in this line 
+#change GRUB_CMDLINE_LINUX line to include console=tty12
+#we can also add "quiet splash" to GRUB_CMDLINE_LINUX_DEFAULT for a boot splash in this line
+
 
 #to ensure new grub file is applied at reboot 
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
